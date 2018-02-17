d806b185fc548e4584719352ca95d91e73118c12
TacticooLAceoilot <oofreedom4@gmail.com>
2018-02-17 11:14:47 +0700

    Editint

---
+++ Assets/Box.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+
+    float rotationX;
+    float rotationY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotationX += Time.deltaTime * 100;
+        rotationY += Time.deltaTime * 180;
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
+	}
+}
